bb683e402b260cf79fbce17775bc56fb33f09992
Marleen <aan_marleen@hotmail.com>
2019-05-18 23:56:07 +0200

    color

--- src/components/TextBox.vue
+++ src/components/TextBox.vue
@@ -20,7 +20,7 @@ export default {
     width: 20%;
     padding: 5%;
     box-sizing: border-box;
-    box-shadow: 0px 0px 10px 5px rgba($darkblue, 0.5); //lengt hor, length ver, blur, spread
+    box-shadow: 0px 0px 10px 5px rgba($darkblue, 0.55); //lengt hor, length ver, blur, spread
     border-radius: 25px;
 }
 
