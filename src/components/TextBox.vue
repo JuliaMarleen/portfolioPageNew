1126e04bb5851cbfdd785b4eaccd2352dbea7c0f
Marleen <aan_marleen@hotmail.com>
2019-05-18 23:44:01 +0200

    textbox

--- src/components/TextBox.vue
+++ src/components/TextBox.vue
@@ -12,10 +12,14 @@ export default {
 
 <style lang="scss" scoped>
 .box{
-    margin: 20%;
-    background-color: rgb(185, 185, 185);
+    margin: 10%;
+    background-color: rgb(224, 224, 224);
     height: 40%;
-    width: 40%;
+    width: 20%;
+    padding: 5%;
+    box-sizing: border-box;
+    box-shadow: 10px 10px 15px 1px rgba(114,167,194,0.6);
+    border-radius: 25px;
 }
 
 </style>
