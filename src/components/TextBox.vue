3f909aadca35500d39b225439855ae0f4c22a01c
Marleen <aan_marleen@hotmail.com>
2019-05-18 23:55:08 +0200

    shadowcolor

--- src/components/TextBox.vue
+++ src/components/TextBox.vue
@@ -11,6 +11,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '../assets/variables';
+
 .box{
     margin: 3%;
     background-color: rgb(224, 224, 220);
@@ -18,7 +20,7 @@ export default {
     width: 20%;
     padding: 5%;
     box-sizing: border-box;
-    box-shadow: 0px 0px 10px 4px rgba(49, 150, 190, 0.596); //lengt hor, length ver, blur, spread
+    box-shadow: 0px 0px 10px 5px rgba($darkblue, 0.5); //lengt hor, length ver, blur, spread
     border-radius: 25px;
 }
 
